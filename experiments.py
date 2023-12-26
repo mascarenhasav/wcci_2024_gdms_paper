ecf35cfc66f512f04c3ac954e07793484c734dac
mascarenhasav <mascarenhasav@gmail.com>
2023-12-26 13:38:58 +0900

    diversity

--- experiments.py
+++ experiments.py
@@ -4,15 +4,15 @@ import json
 import os
 import shutil
 
-NPEAKS = [1, 5, 10, 15, 20, 25, 30, 35, 50, 45, 50]
+NPEAKS = [1, 10, 35]
 NOUT = [0]
 NDIM = [2, 5, 10, 15, 20, 25, 35, 40, 45, 50]
 #NPEAKS = [5]
 #NOUT = [0]
-#NDIM = [2, 5, 10, 25]
+#NDIM = [2]
 METRICS = ["dmid", "dpw", "dtd", "dmi", "dmst", "drad"]
 parameters = {}
-parameters["NRUNS"] = 3
+parameters["NRUNS"] = 50
 parameters["NGEN"] = 100
 parameters["NP"] = 100
 parameters["GOUT"] = [25, 50, 75]
@@ -27,12 +27,12 @@ for peak in NPEAKS:
             parameters["NPEAKS"] = peak
             parameters["NOUT"] = out
             parameters["NDIM"] = dim
-            path = f"{peak}-{dim}"
-            if(os.path.isdir(path) == False):
-                os.mkdir(path)
-            else:
+            path = f"wcci_2024_experiments"
+            if(os.path.isdir(path)):
                 shutil.rmtree(path)
-                os.mkdir(path)
-            with open(f"{path}/config.ini", "w") as outfile: 
+            os.mkdir(path)
+            path += f"/{peak}-{dim}"
+            os.mkdir(path)
+            with open(f"{path}/config.ini", "w") as outfile:
                 json.dump(parameters, outfile)
             os.system(f"python3 benchmark/benchmark.py -p {path} &")
