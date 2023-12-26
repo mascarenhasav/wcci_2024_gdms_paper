8d141b1fec80ef436771eb12de51ae6f01cd0b4b
mascarenhasav <mascarenhasav@gmail.com>
2023-12-26 14:08:25 +0900

    diversity

--- experiments.py
+++ experiments.py
@@ -4,34 +4,35 @@ import json
 import os
 import shutil
 
-NPEAKS = [1, 10, 35]
-NOUT = [0]
-NDIM = [2, 5, 10, 15, 20, 25, 35, 40, 45, 50]
-#NPEAKS = [5]
-#NOUT = [0]
-#NDIM = [2]
-METRICS = ["dmid", "dpw", "dtd", "dmi", "dmst", "drad"]
-parameters = {}
-parameters["NRUNS"] = 50
-parameters["NGEN"] = 100
-parameters["NP"] = 100
-parameters["GOUT"] = [25, 50, 75]
-parameters["LD"] = [0, 100]
-parameters["SEED"] =42
-parameters["PLOT"] = 0
 
-for peak in NPEAKS:
-    for out in NOUT:
-        for dim in NDIM:
-            parameters["METRICS_TO_TEST"] = METRICS
+def loadConfigFiles(pathConfig="."):
+    #Read the parameters from the config file
+    parametersFiles = ["parameters.ini"]
+    parameters = {}
+    for file in parametersFiles:
+        if os.path.isfile(f"{pathConfig}/{file}"):
+            with open(f"{pathConfig}/{file}") as f:
+                p0 = list(json.loads(f.read()).items())
+                for i in range(len(p0)):
+                    parameters[f"{p0[i][0]}"] = p0[i][1]
+        else:
+            print(f"{file}", "FILE_NOT_FOUND", f"The {file} file is mandatory!")
+    return parameters
+
+parameters = loadConfigFiles()
+
+path1 = f"wcci_2024_experiments"
+if(os.path.isdir(path1)):
+    shutil.rmtree(path1)
+os.mkdir(path1)
+
+for peak in parameters["NPEAKS"]:
+    for out in parameters["NOUT"]:
+        for dim in parameters["NDIM"]:
             parameters["NPEAKS"] = peak
             parameters["NOUT"] = out
             parameters["NDIM"] = dim
-            path = f"wcci_2024_experiments"
-            if(os.path.isdir(path)):
-                shutil.rmtree(path)
-            os.mkdir(path)
-            path += f"/{peak}-{dim}"
+            path = f"{path1}/{peak}-{dim}"
             os.mkdir(path)
             with open(f"{path}/config.ini", "w") as outfile:
                 json.dump(parameters, outfile)
